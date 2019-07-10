978a300fd9e630dd6f76eeedd71613a9915403d9
Adam Humpherys <adam@adamh.us>
2019-07-10 15:50:05 -0600

    got login form working, just have to finish 2FA and login logic

--- src/kpfw/kpfw/Controllers/AccountController.cs
+++ src/kpfw/kpfw/Controllers/AccountController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Google.Authenticator;
 using kpfw.DataModels;
 using kpfw.Models;
+using kpfw.Services;
+using kpfw.Services.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -28,7 +31,7 @@ namespace kpfw.Controllers
                 string c = Request.Cookies["InvalidTries"];
                 if (c == null)
                 {
-                    Response.Cookies.Append("InvalidTries", c, new CookieOptions { Expires = DateTime.Now.AddHours(1) });
+                    Response.Cookies.Append("InvalidTries", value.ToString(), new CookieOptions { Expires = DateTime.Now.AddHours(1) });
                 }
                 else
                 {
@@ -37,8 +40,8 @@ namespace kpfw.Controllers
             }
         }
         private DataContext _context;
-        private SignInManager<User> _signInManager;
-        private UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
         public AccountController(DataContext context, UserManager<User> u, SignInManager<User> s)
         {
             _context = context;
@@ -52,6 +55,7 @@ namespace kpfw.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<ActionResult> Login(LoginModel m)
         {
             if (!ModelState.IsValid)
@@ -78,13 +82,100 @@ namespace kpfw.Controllers
                         TempData["LoginModel"] = JsonConvert.SerializeObject(m);
                         TempData["LoginError"] = true;
                         TempData["ErrorMessage"] = "Sorry, Wade couldn't find anyone on file with those credentials. <a href=\"/Contact\">Contact us</a> if you feel this is in error.";
+                        NumTries = NumTries + 1;
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(new User(), false);
-                        
+                        // check password
+                        Auth auth = new Auth(m.Password, user.UserPassword);
+                        if (!auth.Compare(out bool isMd5))
+                        {
+                            TempData["LoginModel"] = JsonConvert.SerializeObject(m);
+                            TempData["LoginError"] = true;
+                            TempData["ErrorMessage"] = "Sorry, Wade couldn't find anyone on file with those credentials. <a href=\"/Contact\">Contact us</a> if you feel this is in error.";
+                            NumTries = NumTries + 1;
+                        }
+                        if (!String.IsNullOrWhiteSpace(user.TwoFactor))
+                        {
+                            // user has 2FA enabled so show that before logging them in.
+                            TempData["2FAUser"] = $"{user.Id}|{user.UserName}|{user.DisplayName}|{user.TwoFactor}|{isMd5}|{user.IsActive}|{auth.GetNewHash()}".ToBase64();
+                            TempData["Show2FA"] = true;
+                        }
+                        //await _signInManager.SignInAsync(new User(), false);
                     }
                 }
+            }
+
+            return Redirect(Request.Form["loginPage"]);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Verify2FA()
+        {
+            // 0 - userId
+            // 1 - userName
+            // 2 - displayName
+            // 3 - Authy ID
+            // 4 - md5
+            // 5 - isActive
+            // 6 - NewHash
+            string[] u = Request.Form["2FAUser"][0].FromBase64().Split('|');
+            bool md5 = Convert.ToBoolean(u[4]), isActive = Convert.ToBoolean(u[5]);
+            //var u = User.GetPassword(txtUserName.Text);
+            bool tfaValid = false;
+
+            if (!Regex.IsMatch(u[3], @"^[\d]+$"))
+            {
+                TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+                if (tfa.ValidateTwoFactorPIN(u[3], Request.Form["TwoFactorCode"][0]))
+                    tfaValid = true;
+            }
+            else
+            {
+                var client = new AuthyClient("");
+                if (client.VerifyToken(Convert.ToInt32(u[3]), Convert.ToInt32(Request.Form["TwoFactorCode"][0])))
+                    tfaValid = true;
+            }
+
+            if (tfaValid)
+            {
+                if ((md5 && !isActive) || isActive)
+                {
+                    await _signInManager.SignInAsync(new User(), false);
+                    //FormsAuthentication.RedirectFromLoginPage(u[1], false);
+                    if (NumTries > 0)
+                    {
+                        Response.Cookies.Delete("InvalidTries");
+                    }
+
+                    if (md5)
+                    {
+                        var user = _context.Users.Where(x => x.Id == Convert.ToInt32(u[0])).SingleOrDefault();
+                        if (user != null)
+                        {
+                            user.UserPassword = u[6];
+                            user.EmailConfirmation = Guid.NewGuid();
+                            user.IsActive = true;
+                            _context.Users.Update(user);
+                        }
+                    }
+                }
+                else
+                {
+                    TempData["2FAError"] = true;
+                    //ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "LoginError", "$.magnificPopup.open({ items: { src: '#loginModalPopup' }, prependTo:'form#aspnetForm', closeOnBgClick: false });", true);
+                    TempData["2FAErrorMessage"] = "Sorry, there's a problem with your account. <a href=\"/Contact\">Contact us</a> to get it resolved.";
+                    //FailureText.Visible = true;
+                    NumTries = NumTries + 1;
+                }
+            }
+            else
+            {
+                // Display error and re-display popup
+                TempData["2FAError"] = true;
+                TempData["2FAErrorMessage"] = "Invalid token. Please try again.";
+                //TwoFactorError.InnerText = "Invalid token. Please try again.";
+                //ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "changePassword", "$.magnificPopup.open({ items: { src: '#twoFAModal' }, prependTo:'form#aspnetForm', closeOnBgClick: false });", true);
             }
 
             return Redirect(Request.Form["loginPage"]);
---
+++ src/kpfw/kpfw/Services/Authentication/Auth.cs
@@ -0,0 +1,83 @@
+﻿using CryptSharp;
+using System.Text;
+
+namespace kpfw.Services.Authentication
+{
+    /// <summary>
+    /// Summary description for Auth
+    /// </summary>
+    public class Auth
+    {
+        private readonly string _md5Hash;
+        private readonly string _hash;
+        private readonly string _pwd;
+        private readonly string _dbHash;
+
+        /// <summary>
+        /// Instantiate Auth object
+        /// </summary>
+        /// <param name="password">User provided password</param>
+        /// <param name="hash">Database hash. For older accounts, this will be MD5. For newer/updated accounts, it will be more secure.</param>
+        public Auth(string password, string hash)
+        {
+            _pwd = password.ToBase64();
+            _dbHash = hash;
+            _md5Hash = CalculateMD5Hash(password);
+            _hash = Crypter.Sha512.Crypt(password);
+        }
+
+        /// <summary>
+        /// Instantiate Auth object
+        /// </summary>
+        /// <param name="password">User provided password</param>
+        public Auth(string password)
+        {
+            _pwd = password.ToBase64();
+            _md5Hash = CalculateMD5Hash(password);
+            _hash = Crypter.Sha512.Crypt(password);
+        }
+
+        /// <summary>
+        /// See if the password is correct
+        /// </summary>
+        /// <param name="isMD5">Lets the application know if the database is still using an MD5 hash</param>
+        /// <returns>True if the password is correct, false if it is not</returns>
+        public bool Compare(out bool isMD5)
+        {
+            bool isValid = false;
+            isMD5 = _dbHash.Length == 32 && !_dbHash.Contains("$");
+            if (!isMD5)
+                isValid = Crypter.CheckPassword(_pwd.FromBase64(), _dbHash);
+            else
+                isValid = _md5Hash == _dbHash;
+
+            return isValid;
+        }
+
+        /// <summary>
+        /// Returns hash of a given password (not MD5). This can be used to update a user's password as well.
+        /// </summary>
+        /// <returns>Hash as a string</returns>
+        public string GetNewHash()
+        {
+            return _hash;
+        }
+
+        private static string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            var md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+
+            return sb.ToString().ToLower();
+        }
+    }
+}
