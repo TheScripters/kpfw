7f0d27c62a0de4643e8379aca67a4a6d2fa943e6
Adam Humpherys <adam@adamh.us>
2021-01-29 10:21:38 -0700

    got pages working

--- src/kpfw/kpfw/Controllers/PageController.cs
+++ src/kpfw/kpfw/Controllers/PageController.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using kpfw.DataModels;
 using kpfw.Models;
+using kpfw.Services;
+using Markdig;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace kpfw.Controllers
 {
     public class PageController : Controller
     {
+        protected MarkdownPipeline pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseCaps().Build();
         readonly DataContext context;
 
         public PageController(DataContext _context)
@@ -31,7 +35,24 @@ namespace kpfw.Controllers
                 }
             }
 
-            return View(new PageModel { PageName = "Test Page", Url = RouteData.Values["PageUrl"].ToString() });
+            Page p = context.Pages.Where(x => x.Url == RouteData.Values["PageUrl"].ToString()).FirstOrDefault();
+            if (p == null)
+                return StatusCode(404);
+
+            return View(new PageModel { PageName = p.Name, Url = p.Url, Title = p.Title, Content = Markdown.ToHtml(p.Content, pipeline) });
+        }
+
+        [Route("/HandleError/{code:int}")]
+        public IActionResult HandleError(int code)
+        {
+            switch (code)
+            {
+                case 404:
+                    string url = Request.GetDisplayUrl();
+                    return View("~/Views/Shared/404.cshtml", url);
+                default:
+                    return View("~/Views/Shared/HandleError.cshtml");
+            }
         }
     }
 }
